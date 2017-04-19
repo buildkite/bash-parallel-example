9fca522946a4c842bf33479831927b8d1ef34b55
Keith Pitt <me@keithpitt.com>
2017-04-19 13:17:08 +1000

    Ensure there's always a default queue when uploading the pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,1 +1,1 @@
-cat .buildkite/pipeline.yml | sed "s/\$BUILDKITE_AGENT_META_DATA_QUEUE/$BUILDKITE_AGENT_META_DATA_QUEUE/" | buildkite-agent pipeline upload
+cat .buildkite/pipeline.yml | sed "s/\$BUILDKITE_AGENT_META_DATA_QUEUE/${BUILDKITE_AGENT_META_DATA_QUEUE:-default}/" | buildkite-agent pipeline upload
